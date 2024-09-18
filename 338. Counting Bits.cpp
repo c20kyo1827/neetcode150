5af97fc88ecc2e4ae7400b53bc6e57f94cd84d6b
Allen Kuan <33478517+c20kyo1827@users.noreply.github.com>
2024-09-18 20:54:03 +0800

    Update 338. Counting Bits.cpp

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -18,15 +18,25 @@
 //         return count;
 //     }
 // };
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         if(n == 0) return std::vector<int>{0};
+//         std::vector<int> ans(n+1,0);
+//         ans[0] = 0;
+//         ans[1] = 1;
+//         for(int i=2 ; i<=n ; i++){
+//             ans[i] = ans[i/2] + ans[i%2];
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        if(n == 0) return std::vector<int>{0};
-        std::vector<int> ans(n+1,0);
-        ans[0] = 0;
-        ans[1] = 1;
-        for(int i=2 ; i<=n ; i++){
-            ans[i] = ans[i/2] + ans[i%2];
+    vector<int> countBits(int num) {
+        vector<int> ans(num + 1, 0);
+        for (int i = 1; i <= num; ++i) {
+            ans[i] = ans[i & (i - 1)] + 1;
         }
         return ans;
     }
