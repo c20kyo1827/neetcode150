36101b49625b95a0707628d3af23870ae12c5740
Allen Kuan <33478517+c20kyo1827@users.noreply.github.com>
2024-10-02 21:30:57 +0800

    Update 268. Missing Number.cpp

--- 268. Missing Number.cpp
+++ 268. Missing Number.cpp
@@ -8,6 +8,14 @@ public:
         // }
         // return nums.size();
 
+        // // XOR
+        // int ans = nums.size();
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     ans ^= i;
+        //     ans ^= nums[i];
+        // }
+        // return ans;
+
         // Trapezoid formula method
         int tf = (0 + nums.size())*(nums.size()+1)/2;
         for(auto num : nums){
