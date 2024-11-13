c85455078a785519ebec0f1238cf0738431dee70
Allen Kuan <33478517+c20kyo1827@users.noreply.github.com>
2024-11-13 19:32:01 +0800

    Create 217. Contains Duplicate.cpp

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        std::unordered_set<int> s;
+        for(auto& num : nums){
+            if(s.count(num)) return true;
+            s.insert(num);
+        }
+        return false;
+    }
+};
